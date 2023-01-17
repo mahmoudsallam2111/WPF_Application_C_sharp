9dee6c1b5b84312930faa56aecc44977216f40ce
unknown <mahmoudsallam2111@gmail.com>
2023-01-17 19:03:04 +0200

    simple calcultor using WPF

---
+++ Calculator App/calc app/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calc_app
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        float fnum;
+        float snum;
+        char opration;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void btn_click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            txt_res.Text += btn.Content.ToString();
+            snum = float.Parse(txt_res.Text);
+
+        }
+
+        private void sum_btn_Click(object sender, RoutedEventArgs e)
+        {
+            fnum = float.Parse(txt_res.Text);
+            opration = '+';
+            txt_res.Clear();
+
+        }
+
+        private void sub_btn_Click(object sender, RoutedEventArgs e)
+        {
+            fnum = float.Parse(txt_res.Text);
+            opration = '-';
+            txt_res.Clear();
+        }
+
+        private void mul_btn_Click(object sender, RoutedEventArgs e)
+        {
+            fnum = float.Parse(txt_res.Text);
+            opration = 'x';
+            txt_res.Clear();
+        }
+
+        private void div_btn_Click(object sender, RoutedEventArgs e)
+        {
+            fnum = float.Parse(txt_res.Text);
+            opration = '/';
+            txt_res.Clear();
+        }
+
+        private void equal_btn_Click(object sender, RoutedEventArgs e)
+        {
+            float result = 0;
+            switch (opration)
+            {
+                case '+':
+                    result = fnum + snum;
+                 break;
+                case '-':
+                    result = fnum - snum;
+                    break;
+                case 'x':
+                    result = fnum * snum;
+                    break;
+                default:
+                    result = fnum / snum;
+                    break;
+
+            }
+            if (result==0)
+            {
+                txt_res.Clear();
+            }
+            else
+            {
+                txt_res.Text = result.ToString();
+            }
+        }
+    }
+}
